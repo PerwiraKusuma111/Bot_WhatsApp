df46f0a9328ac6e8f5445b61a8d1b248c0d077ad
Lawliet <94941593+PerwiraKusuma111@users.noreply.github.com>
2021-12-08 15:04:02 +0700

    Update Perwira.js

--- Perwira.js
+++ Perwira.js
@@ -1415,14 +1415,14 @@ uptime = process.uptime();
 					const userXp = getLevelingXp(sender)
 					/*if (userLevel === undefined && userXp === undefined) return console.log("noregist")*/
 					const requiredXp = 1000 * (Math.pow(2, userLevel) - 
-                if (!isGroup) {
+     if (!isGroup) {
 		if (!command) {
-		if (!isMedia) { 
-                if (mek.key.fromMe) return
+		if (!isMedia) {
+			if (mek.key.fromMe) return
 		siminya = await fetchJson(`https://api-sv2.simsimi.net/v2/?text=${pop}&lc=id`)
 		msgsimi = siminya.success
-		conn.sendMessage(from, `${msgsimi}\n                                   _ˢⁱᵐⁱ ᵐᵉˢˢᵃᵍᵉ_`, text, {quoted: mek, sendEphemeral: true, contextInfo : {forwardingScore: 508, isForwarded: true}})
-		}}}
+		conn.sendMessage(from, `${msgsimi}\n                                   _ᴬᵘᵗᵒ ᵐᵉˢˢᵃᵍᵉ_`, text, {quoted: mek, sendEphemeral: true, contextInfo : {forwardingScore: 508, isForwarded: true}})
+		}}} 
 		
             switch(command){
             	case 'simi_on':
